1973663040246e40686385a65ad7dd3042de5c6b
Thiago Lopes <programadorthiagolopes@gmail.com>
2023-08-22 11:08:32 -0300

    fix: Style

--- src/Screens/Login/index.tsx
+++ src/Screens/Login/index.tsx
@@ -1,20 +1,41 @@
-import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Text, Image, TextInput, TouchableOpacity, Pressable } from 'react-native'
+import React, { useState } from 'react'
 import { style } from './style'
 
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
 export default function Login() {
+    const [show, setShow] = useState(true);
     return (
         <View style={style.main}>
             <Image source={require('../../Images/logo.png')} style={style.image} />
 
-            <TextInput style={style.textInput} placeholder='Email' autoComplete='email'/>
-            <TextInput style={style.textInput} placeholder='Password' autoComplete='current-password'/>
+            <View style={style.content}>
+                <TextInput style={style.textInput} placeholder='Email' autoComplete='email' />
+            </View>
+            <View style={style.contentPassword}>
+                <TextInput style={style.textInput} placeholder='Password' autoComplete='current-password' secureTextEntry={show}/>
+                <TouchableOpacity onPress={() => setShow(!show)}>
+                    {
+                        show ?
+                        <MaterialCommunityIcons name="eye-off" size={24} style={{ marginLeft: -30, marginBottom: 9 }} />
+                        :
+                        <MaterialCommunityIcons name="eye" size={24} style={{ marginLeft: -30, marginBottom: 9 }} />
+                    }
+                </TouchableOpacity>
+            </View>
 
-            <TouchableOpacity style={style.buttonLogin} onPress={() => alert("Login")}>
+            <Pressable>
+                <Text style={style.forgotPassword}>
+                    Forgot Password?
+                </Text>
+            </Pressable>
+
+            <Pressable style={style.buttonLogin} onPress={() => alert("Login")}>
                 <Text>
                     Login
                 </Text>
-            </TouchableOpacity>
+            </Pressable>
         </View>
     )
 }
