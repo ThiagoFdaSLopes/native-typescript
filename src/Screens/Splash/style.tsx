284517f33c7c93eccc649c95d94ae2855d191cdd
Thiago Lopes <programadorthiagolopes@gmail.com>
2023-08-22 09:11:08 -0300

    feat: SplashScreen Style

---
+++ src/Screens/Splash/style.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+const style = StyleSheet.create({
+    main: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#820AD1',
+        width: '100%',
+    }
+});
+
+export default style;
