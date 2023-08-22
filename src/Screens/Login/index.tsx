0b4b9f59cde676631097bfba3d8a3b96baefcd3a
Thiago Lopes <programadorthiagolopes@gmail.com>
2023-08-22 10:31:53 -0300

    feat: Login Component

--- src/Screens/Login/index.tsx
+++ src/Screens/Login/index.tsx
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { style } from './style'
 
-export default function index() {
-  return (
-    <View>
-      <Text>index</Text>
-    </View>
-  )
+export default function Login() {
+    return (
+        <View style={style.main}>
+            <Image source={require('../../Images/logo.png')} style={style.image} />
+
+            <TextInput style={style.textInput} placeholder='Email' autoComplete='email'/>
+            <TextInput style={style.textInput} placeholder='Password' autoComplete='current-password'/>
+
+            <TouchableOpacity style={style.buttonLogin} onPress={() => alert("Login")}>
+                <Text>
+                    Login
+                </Text>
+            </TouchableOpacity>
+        </View>
+    )
 }
