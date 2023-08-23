101ef8ddf5aa4a588e8eeda9b7e981f78010ed00
Thiago Lopes <programadorthiagolopes@gmail.com>
2023-08-23 10:36:53 -0300

    feat: Splash Screen to Login

--- src/Screens/Splash/index.tsx
+++ src/Screens/Splash/index.tsx
@@ -1,16 +1,30 @@
 import { View, Text, StatusBar, Image } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import style from './style'
+import { useNavigation } from '@react-navigation/native';
 
-const SplashScreen = () => {
-  return (
-      <>
-          <View style={style.main}>
-              <Image source={require('../../Images/logo.png')} />
-          </View>
-          <StatusBar backgroundColor={"#820AD1"}  barStyle={'light-content'} />
-      </>
-  )
+const SplashScreen = ({ }) => {
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            navigation.navigate('Login' as never)
+        }, 3000)
+
+        return () => {
+            clearInterval(interval)
+        }
+    }, [navigation])
+
+
+    return (
+        <>
+            <View style={style.main}>
+                <Image source={require('../../Images/logo.png')} />
+            </View>
+            <StatusBar backgroundColor={"#820AD1"} barStyle={'light-content'} />
+        </>
+    )
 }
 
 export default SplashScreen;
