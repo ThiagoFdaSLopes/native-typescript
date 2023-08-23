f18b43837ac1774c8051e539bc94ca1ff1896fa7
Thiago Lopes <programadorthiagolopes@gmail.com>
2023-08-23 09:42:47 -0300

    feat: Routes index

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,10 +1,17 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+
+import AuthRoutes from './AuthRoutes/AuthRoutes';
+import AppRoutes from './AppRoutes/AppRoutes';
 
 export default function Routes() {
-  return (
-    <View>
-      <Text>Routes</Text>
-    </View>
-  )
+    const user = false;
+    return (
+        <NavigationContainer>
+            {
+                user ? <AppRoutes /> : <AuthRoutes />
+            }
+        </NavigationContainer>
+    )
 }
