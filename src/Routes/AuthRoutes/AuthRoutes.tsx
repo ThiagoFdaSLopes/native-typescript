cb4c60df3855078f21fbd468c015eacb13bd2c85
Thiago Lopes <programadorthiagolopes@gmail.com>
2023-08-24 09:50:13 -0300

    fix:  SplashScreen

--- src/Routes/AuthRoutes/AuthRoutes.tsx
+++ src/Routes/AuthRoutes/AuthRoutes.tsx
@@ -5,14 +5,13 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Login from '../../Screens/Login';
 import Register from '../../Screens/Register';
 import ForgotPassword from '../../Screens/ForgotPassword';
-import SplashScreen from '../../Screens/Splash';
+// import SplashScreen from '../../Screens/Splash';
 
 const Stack = createNativeStackNavigator();
 
 const AuthRoutes = () => {
     return (
-        <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName='SplashScreen'>
-            <Stack.Screen name="SplashScreen" component={SplashScreen} />
+        <Stack.Navigator screenOptions={{ headerShown: false }} >
             <Stack.Screen name="Login" component={Login} />
             <Stack.Screen name="Register" component={Register} />
             <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
