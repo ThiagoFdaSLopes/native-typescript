c3885c0d36a852be0e8bf4edd90126d521fbe77f
Thiago Lopes <programadorthiagolopes@gmail.com>
2023-08-26 10:28:40 -0300

    feat: Style MainScreen

---
+++ src/Screens/MainScreen/style.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export const style = StyleSheet.create({
+    main: {
+        flex: 1,
+        backgroundColor: '#820ad1',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+    }
+})
