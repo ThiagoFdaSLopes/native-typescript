f3db676d1b7614fb916b80607818e61b0e9fd2cd
Thiago Lopes <programadorthiagolopes@gmail.com>
2023-08-26 10:28:25 -0300

    feat: MainScreen

---
+++ src/Screens/MainScreen/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+import { style } from './style'
+
+export default function MainScreen() {
+  return (
+    <View style={style.main}>
+      <Text>MainScreen</Text>
+    </View>
+  )
+}
